2b4b84de47bad98ff413847fc5455ec86eb35fcb
emeliehansson <emeliehansson_@hotmail.com>
2022-11-01 09:38:20 +0000

    Add first structure to results in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function(){
 
     let rock = document.getElementById('rock');
     let paper = document.getElementById('paper');
-    let scissor = document.getElementById('scissor');
+    let scissor = document.getElementById('scissors');
     let lizard = document.getElementById('lizard');
     let spock = document.getElementById('spock');
     const rpslsOptions = ['Rock', 'Paper', 'Scissor', 'Lizard', 'Spock'];
@@ -29,4 +29,65 @@ function createComputerChoice() {
 function runGame(userChoice) {
     let computerChoice = createComputerChoice();
     displayResult(userChoice, computerChoice);
+}
+
+/**
+ * score variables, will be called in function
+ * to be able to update score result
+ */
+
+let updateScore = () => {
+    let userScoreDisplay = document.getElementById('user-score');
+    userScoreDisplay.innerHTML = userScore;
+    let computerScoreDisplay = document.getElementById('computer-score');
+    computerScoreDisplay.innerHTML = computerScore;
+}
+
+// results to be displayed and to update score
+
+function displayResult(userChoice, computerChoice) {
+    if (userChoice === computerChoice) {
+        result.innerHTML = "It's a tie!! 😬";
+        userScore++;
+        computerScore++;
+        updateScore();
+    } else if (userChoice === scissors && computerChoice === paper) {
+        result.innerHTML = 'Scissor cuts paper, you win!! ⭐';
+        userScore++;
+        updateScore();
+    } else if (userChoice === paper && computerChoice === rock) {
+        result.innerHTML = 'Paper covers rock, you win!! ⭐';
+        userScore++;
+        updateScore();
+    } else if (userChoice === rock && computerChoice === lizard) {
+        result.innerHTML = 'Rock crushes lizard, you win!! ⭐';
+        userScore++;
+        updateScore();
+    } else if (userChoice === lizard && computerChoice === spock) {
+        result.innerHTML = 'Lizard poisons Spock, you win!! ⭐';
+        userScore++;
+        updateScore();
+    } else if (userChoice === spock && computerChoice === scissors) {
+        result.innerHTML = 'Spock smashes scissors, you win!! ⭐';
+        userScore++;
+        updateScore();
+    } else if (userChoice === scissors && computerChoice === lizard) {
+        result.innerHTML = 'Scissors decapitates lizard, you win!! ⭐';
+        userScore++;
+        updateScore();
+    } else if (userChoice === lizard && computerChoice === paper) {
+        result.innerHTML = 'Lizard eats paper, you win!! ⭐';
+        userScore++;
+        updateScore();
+    } else if (userChoice === paper && computerChoice === spock) {
+        result.innerHTML = 'Paper disproves Spock, you win!! ⭐';
+        userScore++;
+        updateScore();
+    } else if (userChoice === spock && computerChoice === rock) {
+        result.innerHTML = 'Spock vaporizes rock, you win!! ⭐';
+        userScore++;
+        updateScore();
+    } else if (userChoice === rock && computerChoice === scissors) {
+        result.innerHTML = 'Rock crushes scissors, you win!! ⭐'
+    }
 }
