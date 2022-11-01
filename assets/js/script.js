05867e18b3ffec6ddf6c038b39e294036c0d2f9f
emeliehansson <emeliehansson_@hotmail.com>
2022-11-01 08:13:38 +0000

    Add first structure to javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,32 @@
 // DOM variables
 
+document.addEventListener("DOMContentLoaded", function(){
+    let buttons = document.getElementsByTagName('button');
+
+    let rock = document.getElementById('rock');
+    let paper = document.getElementById('paper');
+    let scissor = document.getElementById('scissor');
+    let lizard = document.getElementById('lizard');
+    let spock = document.getElementById('spock');
+    const rpslsOptions = ['Rock', 'Paper', 'Scissor', 'Lizard', 'Spock'];
+
+    let result = document.getElementById('result');
+
+    let userScore = 0;
+    let computerScore = 0;
+    
+})
+
+// random choice for computer function
+
+function createComputerChoice() {
+    let randomChoice = Math.floor(Math.random() * 5);
+    return rpslsOptions[randomChoice];
+}
+
+// run game function
+
+function runGame(userChoice) {
+    let computerChoice = createComputerChoice();
+    displayResult(userChoice, computerChoice);
+}
